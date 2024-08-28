f681fa57ae3c99d4905b75f08c7a347436f38492
joezabinski-wellsky <joe.zabinski@wellsky.com>
2024-08-28 11:38:38 -0500

    Module federation installed

--- shell/next.config.mjs
+++ shell/next.config.mjs
@@ -1,6 +1,23 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          filename: 'static/chunks/remoteEntry.js',
+          name: 'shell',
+          remotes: {
+            angularEmbedded:"angularEmbedded@http://localhost:3002/remoteEntry.js",
+          }
+        }),
+      );
+    }
+
+    return config;
+  }
 };
 
 export default nextConfig;
