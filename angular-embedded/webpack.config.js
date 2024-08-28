6f34e5d9a247bdd644d6c5b84430cb756e060d8e
joezabinski-wellsky <joe.zabinski@wellsky.com>
2024-08-28 08:58:10 -0500

    Cleaned up webpack

--- angular-embedded/webpack.config.js
+++ angular-embedded/webpack.config.js
@@ -1,57 +1,22 @@
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
-
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, 'tsconfig.json'),
-  [/* mapped paths to share */]);
 
 module.exports = {
+  // Makes remote.entry.js loadable through Webpack
   output: {
-    uniqueName: "angular-embedded",
-    publicPath: "auto",
+    publicPath: "auto",  // documentation says this is the default, but it's not
     scriptType:'text/javascript'
   },
+  // Webpack apparently optimizes away needed Webpack.require functions without the below setting
   optimization: {
-    runtimeChunk: false
-  },
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
+    runtimeChunk: false // documentation says this is the default, but it's not
   },
   plugins: [
     new ModuleFederationPlugin({
-        // library: { type: "module" },
-
-        // For remotes (please adjust)
         name: "angularEmbedded",
         filename: "remoteEntry.js",
         exposes: {
             './angularModule':'./src/loadApp.ts'
-        },
-
-        // For hosts (please adjust)
-        // remotes: {
-        //     "mfe1": "http://localhost:3000/remoteEntry.js",
-
-        // },
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-
+        }
     }),
-    sharedMappings.getPlugin()
   ],
 };
